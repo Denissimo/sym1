53e99bbf1398a8a79a586e86575dd78a117a02d6
Den <droden@narod.ru>
2018-11-24 20:09:32 +0300

    report data

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -10,12 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Proxy;
 use App\Twig\Render;
+use Doctrine\Common\Collections\Criteria;
 use App\Api\RequestAPI\RequestAPI;
 use App\Api\Slovo\Api4s;
 
 
 class ReportController extends BaseController
 {
+    const
+        DAYS = 'P380D',
+        MAX = 1000
+    ;
 
     /**
      * @Route("report", name="report")
@@ -24,9 +29,20 @@ class ReportController extends BaseController
     public function report()
     {
 
-        Proxy::init()->getConnecton()->query();
+//        $query = 'SELECT * FROM apps a LEFT JOIN users u ON a.user_id = u.id WHERE DATE(a.createdAt) > (CURDATE() - '.self::DAYS.')';
+//        $data['report'] = Proxy::init()->getConnecton()->query($query)->fetchAll();
+        $criteria = Criteria::create();
+        $data = Proxy::init()->getEntityManager()->getRepository(\Apps::class)->matching(
+            $criteria->where(
+                Criteria::expr()->gte('createdat', (new \DateTime())->sub(new \DateInterval(self::DAYS))
+                )
+            )
+            ->orderBy(['id' => Criteria::DESC])
+            ->setMaxResults(self::MAX)
+        )->toArray();
+//        var_dump($data);
 
-        return (new Render())->simpleRender(['data' => $e->getMessage()], 'test.html.twig');
+        return (new Render())->simpleRender(['data' => $data], 'report.html.twig');
     }
 
 }
