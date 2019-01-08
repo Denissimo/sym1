087c8843aed2113bae70343494cd922160eda95c
Den <droden@narod.ru>
2019-01-08 14:23:20 +0300

    repotr timefilter

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -13,11 +13,13 @@ use App\Twig\Render;
 use Doctrine\Common\Collections\Criteria;
 use App\Api\RequestAPI\RequestAPI;
 use App\Api\Slovo\Api4s;
+use App\Controller\MainController as Controller;
 
 
 class ReportController extends BaseController
 {
     const
+        TPL_DATE_TIME = 'd.m.Y',
         QTY = 'qty',
         NAME = 'name',
         NEW = 'new',
@@ -26,6 +28,7 @@ class ReportController extends BaseController
         SUMM_LABEL = 'Итого',
         READY = 'ready',
         DAYS = 'P380D',
+        AND = ' AND ',
         MAX = 1000;
 
     /**
@@ -53,9 +56,10 @@ class ReportController extends BaseController
      */
     public function statReport()
     {
-
         $query = 'SELECT p.id AS pid, p.title, a.in_work, a.status, `as`.picture, a.trash,
-COUNT(a.id) AS qty FROM apps a LEFT JOIN partners p ON a.partner_id = p.id
+COUNT(a.id) AS qty FROM 
+  (SELECT * FROM apps ' . $this->buildPeriod() . ') a 
+LEFT JOIN partners p ON a.partner_id = p.id
 LEFT JOIN app_status `as` ON a.status=`as`.id GROUP BY p.title, a.status, a.in_work, a.trash';
         $statReport = Proxy::init()->getConnecton()->query($query)->fetchAll();
         $statTable = [];
@@ -105,16 +109,52 @@ LEFT JOIN app_status `as` ON a.status=`as`.id GROUP BY p.title, a.status, a.in_w
         }
         $statTable[self::SUMM] = $summ;
 
+        $data['request'] = self::getRequest()->query->all();
         $data['statTable'] = $statTable;
         $data['statList'] = [
             self::ALL => [self::NAME => self::ALL, \AppStatus::PICTURE => 'Все&nbsp;<img src="/images/color_labels/color_all.png" class="color_label">'],
             self::NEW => [self::NAME => self::NEW, \AppStatus::PICTURE => 'Новые&nbsp;<img src="/images/color_labels/color_white.png" class="color_label">'],
             \Apps::IN_WORK => [self::NAME => \Apps::IN_WORK, \AppStatus::PICTURE => 'В&nbsp;работе&nbsp;<img src="/images/color_labels/color_cyan.png" class="color_label">'],
             self::READY => [self::NAME => self::READY, \AppStatus::PICTURE => 'Готовые&nbsp;<img src="/images/color_labels/color_green.png" class="color_label">'],
-            \Apps::TRASH =>  [self::NAME => \Apps::TRASH, \AppStatus::PICTURE => 'Корзина&nbsp;<div class="emoji emoji1f4a9" style="margin: 0 0 8px 0;"></div>'],
+            \Apps::TRASH => [self::NAME => \Apps::TRASH, \AppStatus::PICTURE => 'Корзина&nbsp;<div class="emoji emoji1f4a9" style="margin: 0 0 8px 0;"></div>'],
         ];
 
         return (new Render())->render($data, 'statreport.html.twig');
     }
 
+    /**
+     * @return string|null
+     */
+    private function buildPeriod()
+    {
+        $fields = [
+            self::CREATE_FROM => self::getRequest()->get(self::CREATE_FROM),
+            self::CREATE_TO => self::getRequest()->get(self::CREATE_TO),
+            self::UPDATE_FROM => self::getRequest()->get(self::UPDATE_FROM),
+            self::UPDATE_TO => self::getRequest()->get(self::UPDATE_TO),
+        ];
+
+        $andFields = [];
+        $fields[self::CREATE_FROM] ? $andFields[] =
+            self::CREATE_AT . '>=' . $this->buildDate($fields[self::CREATE_FROM]) : null;
+        $fields[self::CREATE_TO] ? $andFields[] =
+            self::CREATE_AT . '<=' . $this->buildDate($fields[self::CREATE_TO]) : null;
+        $fields[self::UPDATE_FROM] ? $andFields[] =
+            self::UPDATE_AT . '>=' . $this->buildDate($fields[self::UPDATE_FROM]) : null;
+        $fields[self::UPDATE_TO] ? $andFields[] =
+            self::UPDATE_AT . '<=' . $this->buildDate($fields[self::UPDATE_TO]) : null;
+
+        $glued = implode(self:: AND, $andFields);
+
+        return $glued ? ' WHERE ' . $glued : null;
+    }
+
+    /**
+     * @param string $date
+     * @return string
+     */
+    private function buildDate(string $date): string
+    {
+        return 'DATE("' . \DateTime::createFromFormat(self::TPL_DATE_TIME, $date)->format('Y-m-d') . '")';
+    }
 }
